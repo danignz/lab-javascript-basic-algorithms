f915a2933413ddd52a2019d72ed9b49ec23f6af4
Daniel Gonzalez <danielgnzch@gmail.com>
2022-04-19 16:57:19 +0200

    Add Iteration 3 solution

--- js/index.js
+++ js/index.js
@@ -21,3 +21,37 @@ if ( hacker1.length > hacker2.length ){
 
 
 // Iteration 3: Loops
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+let separatedName = '';
+
+for (let i = 0; i < hacker1.length ; i++){
+  separatedName += hacker1[i].toUpperCase();
+  if( i !== hacker1.length-1 ){
+    separatedName += ' ';
+  }
+}
+
+console.log(`${separatedName}`);
+
+//3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+let reversedName = '';
+
+for (let i = hacker2.length-1; i >= 0; i--) {
+  reversedName += hacker2[i];
+}
+
+console.log(`${reversedName}`);
+
+//3.3 Depending on the lexicographic order of the strings, print:
+function checkLexicographicOrder(word1, word2){
+  if(word1.localeCompare(word2) < 0) {
+    console.log(`The driver's name goes first.`);
+  } else if (word1.localeCompare(word2) === 0){
+     console.log(`What?! You both have the same name?`);
+  }else{
+    console.log(`Yo, the navigator goes first definitely.`);
+  }
+}
+
+checkLexicographicOrder(hacker1, hacker2);
+
